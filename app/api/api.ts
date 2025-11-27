7a341a94b26a1e12b537e842dccac0259cc13245
Serhii Haievoi <gaevuha@gmail.com>
2025-11-28 00:28:29 +0200

    fix baseUrl on app/api/api.ts

--- app/api/api.ts
+++ app/api/api.ts
@@ -3,7 +3,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_URL || "https://car-rental-api.goit.global",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   // withCredentials: true,
 });
